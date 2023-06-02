bbf7f89c1031d7f3c1474f9bae82960fff233dbd
hsilnam3 <hsilnam3@gmail.com>
2023-06-02 13:10:57 +0900

    Initial project

---
+++ src/main/java/com/clone/todomate/TodomateApplication.java
@@ -0,0 +1,13 @@
+package com.clone.todomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodomateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/todomate/TodomateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.todomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
